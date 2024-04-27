d82a07c479d2e16bfd971bde83e9eaff9552b36e
walshw <williamdouglaswalsh@gmail.com>
2024-04-26 20:11:27 -0400

    Removed cached case, comments

--- webapp/src/components/teams/team-list/teamList.js
+++ webapp/src/components/teams/team-list/teamList.js
@@ -3,27 +3,14 @@ import TeamCard from '../team-card/teamCard';
 
 import "./teamList.css";
 
-// const teams = [
-//     { name: "William", pickOrder: 1 },
-//     { name: "Billy", pickOrder: 3 },
-//     { name: "Guillermo", pickOrder: 2 },
-//     { name: "Wilhelm", pickOrder: 4 },
-//     { name: "Guglielmo", pickOrder: 5 },
-// ];
-
 const TeamList = (props) => {
-    const isThisBitchReal = (userId, teamId) => {
-        console.log(userId, teamId);
-        return userId === teamId;
-    }
-
     const renderTeamCards = (teams) => {
         teams.sort((team, prev) => team.pickOrder - prev.pickOrder);
 
         return <div className="teamsContainer">
             {teams.map(team => {
                 return <div key={team.id}>
-                    <TeamCard team={team} isCurrentUser={isThisBitchReal(props.userId,team.id)} isPicking={team.id === props.pickingTeamId} />
+                    <TeamCard team={team} isCurrentUser={props.userId === team.id} isPicking={team.id === props.pickingTeamId} />
                 </div>
             })}
         </div>;
