c6e7fd31f373ced5cb517c68305bf73797c9c718
walshw <williamdouglaswalsh@gmail.com>
2025-02-06 17:40:52 -0500

    Styling searchbar, added padding to mainscreencontainer

--- webapp/src/components/search-bar/searchbar.js
+++ webapp/src/components/search-bar/searchbar.js
@@ -1,10 +1,12 @@
 import SlCard from '@shoelace-style/shoelace/dist/react/card';
 import SlInput from '@shoelace-style/shoelace/dist/react/input';
+import SlIcon from '@shoelace-style/shoelace/dist/react/icon';
+import './searchBar.css';
 
 const SearchBar = () => {
-    return <SlCard>
-        <SlInput label="Enter Pokemon Name 🅿">
-
+    return <SlCard className="searchBar">
+        <SlInput placeholder="Search..." >
+            <SlIcon name="search" slot="suffix"></SlIcon>
         </SlInput>
     </SlCard>;
 }
